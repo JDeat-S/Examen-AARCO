d73203db46d4a0821b1cd620861ac114651f577d
Jhovan Alexandro Gallardo Torres <jhovan14000@gmail.com>
2022-10-10 18:21:06 -0500

    Implementacion de webapi y consumo de api Implementacion de base de conexion a la base de datos

--- Examen AARCO/Controllers/CotizacionController.cs
+++ Examen AARCO/Controllers/CotizacionController.cs
@@ -1,18 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Nancy.Json;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace AARCO.Controllers
 {
     public class CotizacionController : Controller
     {
         private readonly ILogger<CotizacionController> _logger;
+        private readonly IConfiguration _config;
+        static HttpClient client = new HttpClient();
+        private readonly string URLbase;
 
-        public CotizacionController(ILogger<CotizacionController> logger)
+        public CotizacionController(ILogger<CotizacionController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
+            URLbase = _config.GetValue<string>("ServicesUrl:WebApi");
         }
 
-        public IActionResult Cotizacion()
+        public async Task<IActionResult> CotizacionAsync()
         {
+       //     string jsonser = JsonConvert.SerializeObject(input);
+            string Serviciosconsulta = URLbase + "/WeatherForecast";
+            var response = await client.GetAsync(Serviciosconsulta);
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            var result = (new JavaScriptSerializer()).DeserializeObject(apiResponse);
+
             return View();
         }
     }
