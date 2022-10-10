3fbed559679a620ae6d23f305f90e2ec32feb923
Jhovan Alexandro Gallardo Torres <jhovan14000@gmail.com>
2022-10-10 16:48:04 -0500

    Termino de vista Cotizacion, Layout y Error Eliminacion de vistas Se agrego modelos de cotizacion y error Cambio de nombre site a Cotizacion en js y css

---
+++ Examen AARCO/Models/Cotizacion.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Examen_AARCO.Models
+{
+    public class Cotizacion
+    {
+        [Display(Name = "Marca: ")]
+        public string Marca { get; set; }
+        [Display(Name = "SubMarca: ")]
+        public string SubMarca { get; set; }
+        [Display(Name = "Modelo: ")]
+        public string Modelo { get; set; }
+        [Display(Name = "Descripcion: ")]
+        public string Descripcion { get; set; }
+        public  string CP { get; set; }
+        public  string Estado { get; set; }
+        [Display(Name = "Municipio: ")]
+        public string Municipio { get; set; }
+        [Display(Name = "Colonia: ")]
+        public  string Colonia { get; set; }
+
+
+    }
+}
---
+++ Examen AARCO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Examen_AARCO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
