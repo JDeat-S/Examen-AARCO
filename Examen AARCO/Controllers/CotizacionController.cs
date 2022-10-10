1cb44beb96dde02c16e287ce500ae659a2a15186
Jhovan Alexandro Gallardo Torres <jhovan14000@gmail.com>
2022-10-10 13:44:44 -0500

    Se crea vista controlador Cotizacion Se actualiza ruta de inicio Se cambia carpeta Pages a Views

---
+++ Examen AARCO/Controllers/CotizacionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AARCO.Controllers
+{
+    public class CotizacionController : Controller
+    {
+        private readonly ILogger<CotizacionController> _logger;
+
+        public CotizacionController(ILogger<CotizacionController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Cotizacion()
+        {
+            return View();
+        }
+    }
+}
